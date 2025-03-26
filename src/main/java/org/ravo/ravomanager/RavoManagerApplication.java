a25b711875c51d1e3bd520c946f254c59160ece8
unknown <judemin2087@naver.com>
2025-03-26 21:44:14 +0900

    [INIT]: Inititalized Project

---
+++ src/main/java/org/ravo/ravomanager/RavoManagerApplication.java
@@ -0,0 +1,13 @@
+package org.ravo.ravomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavoManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ravo/ravomanager/RavoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ravo.ravomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
