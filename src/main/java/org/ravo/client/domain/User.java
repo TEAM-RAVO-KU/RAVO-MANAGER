1783233711b89a785d421471a9eb2e27ced353c5
kkb <gimgyubin1225@gmail.com>
2025-10-06 19:14:42 +0900

    [FEAT] User 회원가입, 로그입 기능 추가

---
+++ src/main/java/org/ravo/client/domain/User.java
@@ -0,0 +1,29 @@
+package org.ravo.client.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long balance = 0L;   // 기본 잔액 0원
+}
