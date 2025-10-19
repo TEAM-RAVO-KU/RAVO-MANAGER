452a632f406b617cd1bd86186afa4202b2d79b0d
kkb <gimgyubin1225@gmail.com>
2025-10-19 14:34:05 +0900

    [REFACTOR] 프로젝트 설정 및 의존성 업데이트
    - WebClient 의존성 추가
    - CORS 설정 수정
    - 다중 DataSource 설정 개선

--- src/main/java/org/ravo/config/WebConfig.java
+++ src/main/java/org/ravo/config/WebConfig.java
@@ -1,5 +1,7 @@
 package org.ravo.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,5 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         // favicon 에러 무시
+    }
+    
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
